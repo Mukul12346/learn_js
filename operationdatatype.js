52b5af8fe290833ec5e72581927de8faee33bbb0
Mukul12346 <154653666+Mukul12346@users.noreply.github.com>
2024-08-03 07:46:29 +0000

    operationdatatype

---
+++ operationdatatype.js
@@ -0,0 +1,34 @@
+// let someNumber=33
+// let stringNumber=string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************************Operation*************************
+let value=3
+let negValue=-value
+console.log(negValue);
+
+ let str1="mukul"
+ let str2=" chudhary"
+
+ let str3=str1+str2
+ console.log(str3);
+
+ console.log("1"+2+2);
+  
+ console.log(1+1+"2");
+
+ console.log(+true);
+ console.log(+"");
+
+//  let num1,num2,num3
+//  num1=num2=num3=2+2
+ let gameCounter=100
+ ++gameCounter;
+ console.log(gameCounter);
+ 
+ 
+
+
+ 
+
