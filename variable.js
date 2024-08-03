4fe989240e0c07521b709d75ce4dd614a0e540ea
Mukul12346 <154653666+Mukul12346@users.noreply.github.com>
2024-08-03 04:52:58 +0000

    1

---
+++ variable.js
@@ -0,0 +1,15 @@
+const accountID=144553
+let accountEmail="js@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountID=2 
+
+console.log(accountID);
+accountEmail="df@gmail.com"
+accountPassword="212121"
+accountCity="noida"
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
